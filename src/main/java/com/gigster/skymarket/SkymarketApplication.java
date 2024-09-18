e0c4663bd380515002f3fcdd7257ad5753bcf77d
Cheruiyot Kelvin Collins <cheruiyotkelvincollins@localhost.localdomain>
2024-09-18 22:47:32 +0300

    project structure setup

---
+++ src/main/java/com/gigster/skymarket/SkymarketApplication.java
@@ -0,0 +1,13 @@
+package com.gigster.skymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkymarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkymarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gigster/skymarket/SkymarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigster.skymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkymarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
