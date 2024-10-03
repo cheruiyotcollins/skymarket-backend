2e27c00d39d12a58c4fe915f68e79ac09b057d38
Cheruiyot Kelvin Collins <cheruiyotkelvincollins@localhost.localdomain>
2024-10-04 00:36:25 +0300

    basic customer service setup

--- src/main/java/com/gigster/skymarket/service/CustomerService.java
+++ src/main/java/com/gigster/skymarket/service/CustomerService.java
@@ -1,7 +1,17 @@
 package com.gigster.skymarket.service;
 
+import com.gigster.skymarket.dto.NewCustomer;
+import com.gigster.skymarket.dto.ResponseDto;
+import com.gigster.skymarket.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+    public ResponseDto saveCustomer(NewCustomer newCustomer){
+        return new ResponseDto();
+
+    }
 }
