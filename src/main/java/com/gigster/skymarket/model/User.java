2d1c9d390810f5abc074a525481b4328c5bbde29
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-18 06:33:04 +0300

    Added username as path of the values

--- src/main/java/com/gigster/skymarket/model/User.java
+++ src/main/java/com/gigster/skymarket/model/User.java
@@ -1,6 +1,7 @@
 package com.gigster.skymarket.model;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,11 +9,13 @@ import java.io.Serializable;
 @Entity
 @Table(name="users")
 @Data
+@Builder
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fullName;
+    private String username;
     private String email;
     private String phoneNo;
     private String password;
