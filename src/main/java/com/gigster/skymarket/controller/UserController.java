3eb3d1626e9350526ee1151a8315f258f4d5d598
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-18 06:33:45 +0300

    Added register method to UserController

--- src/main/java/com/gigster/skymarket/controller/UserController.java
+++ src/main/java/com/gigster/skymarket/controller/UserController.java
@@ -1,6 +1,12 @@
 package com.gigster.skymarket.controller;
 
+import com.gigster.skymarket.dto.SignUpRequest;
+import com.gigster.skymarket.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/user/")
 @Slf4j
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("new")
+    public ResponseEntity<?> register(@RequestBody SignUpRequest signUpRequest){
+        return userService.register(signUpRequest);
+    }
 }
