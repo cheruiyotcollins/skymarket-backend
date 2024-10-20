199284e1714682259352a3700ee0df8fa988b33f
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-21 01:40:35 +0300

    Enforced @ManyToOne Product-Category relationship

--- src/main/java/com/gigster/skymarket/model/Product.java
+++ src/main/java/com/gigster/skymarket/model/Product.java
@@ -6,14 +6,18 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Entity
-@Table(name="products")
+@Table(name = "products")
 @Data
 public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String productName;
-    private String category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
     private String manufacturer;
     private double price;
     private int stock;
