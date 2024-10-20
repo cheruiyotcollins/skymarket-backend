9e16b2e8300f02e8afbc07a6b419b16b96fbc235
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-21 02:26:28 +0300

    changed Product Category from String to Category Entity

--- src/main/java/com/gigster/skymarket/service/ProductService.java
+++ src/main/java/com/gigster/skymarket/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.gigster.skymarket.service;
 
 import com.gigster.skymarket.dto.NewProductDto;
+import com.gigster.skymarket.dto.ResponseDto;
+import com.gigster.skymarket.model.Category;
 import com.gigster.skymarket.model.Product;
+import com.gigster.skymarket.repository.CategoryRepository;
 import com.gigster.skymarket.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,14 +21,18 @@ public class ProductService {
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+    @Autowired
+    CategoryRepository categoryRepository;
+    ResponseDto responseDto;
 
     public Product createProduct(NewProductDto newProductDto) {
         Product product = new Product();
         product.setProductName(newProductDto.getProductName());
-        product.setCategory(newProductDto.getCategory());
         product.setManufacturer(newProductDto.getManufacturer());
         product.setPrice(newProductDto.getPrice());
         product.setStock(newProductDto.getStock());
+        Optional<Category> category=categoryRepository.findById(newProductDto.getCategory_id());
+        category.ifPresent(product::setCategory);
 
         return productRepository.save(product);
     }
@@ -44,10 +51,11 @@ public class ProductService {
         if (optionalProduct.isPresent()) {
             Product product = optionalProduct.get();
             product.setProductName(newProductDto.getProductName());
-            product.setCategory(newProductDto.getCategory());
             product.setManufacturer(newProductDto.getManufacturer());
             product.setPrice(newProductDto.getPrice());
             product.setStock(newProductDto.getStock());
+            Optional<Category> category=categoryRepository.findById(newProductDto.getCategory_id());
+            category.ifPresent(product::setCategory);
             return productRepository.save(product);
         } else {
             throw new RuntimeException("Product not found with id " + id);
