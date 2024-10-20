d58ff51b5e907cd74e3748169dc2a4984d4bd4f6
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-20 06:31:33 +0300

    added User-Role relationship

--- src/main/java/com/gigster/skymarket/model/User.java
+++ src/main/java/com/gigster/skymarket/model/User.java
@@ -5,18 +5,30 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 @Table(name="users")
 @Data
 @Builder
-public class User implements Serializable {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String fullName;
+    private String name;
+    @Column(nullable = false, unique = true)
     private String username;
+    @Column(nullable = false, unique = true)
+//    @Email
     private String email;
-    private String phoneNo;
+
+    @Column(nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
