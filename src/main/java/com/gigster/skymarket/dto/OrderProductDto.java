3925d69ee1a36d2164fec947da9568de6c638d0e
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-21 22:40:10 +0300

    created OrderProductDto

---
+++ src/main/java/com/gigster/skymarket/dto/OrderProductDto.java
@@ -0,0 +1,12 @@
+package com.gigster.skymarket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderProductDto {
+    private Long productId;
+    private int quantity;
+}
+
