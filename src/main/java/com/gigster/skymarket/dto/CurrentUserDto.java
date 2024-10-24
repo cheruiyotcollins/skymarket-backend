c5ba0f9a26feaa6a3689572979f6f5a9472eb56f
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-24 05:20:57 +0300

    added lombok @Builder

--- src/main/java/com/gigster/skymarket/dto/CurrentUserDto.java
+++ src/main/java/com/gigster/skymarket/dto/CurrentUserDto.java
@@ -1,14 +1,12 @@
 package com.gigster.skymarket.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CurrentUserDto {
     private String name;
     private String email;
