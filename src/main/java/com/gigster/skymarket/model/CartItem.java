83edff84338368c36bda0e4e89d08a9c80ff7e24
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-25 03:49:39 +0300

    Created CartItem model

---
+++ src/main/java/com/gigster/skymarket/model/CartItem.java
@@ -0,0 +1,38 @@
+package com.gigster.skymarket.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cart_items")
+public class CartItem {
+
+    // Getters and Setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+    // Calculate subtotal for this item
+    public double getSubtotal() {
+        return this.product.getPrice() * this.quantity;
+    }
+}
+
