9e2d6f83e9bf18707b66ddae2c796cb9c8e2f496
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-25 03:13:15 +0300

    Added email notification to NotificationService

--- src/main/java/com/gigster/skymarket/service/NotificationService.java
+++ src/main/java/com/gigster/skymarket/service/NotificationService.java
@@ -1,10 +1,28 @@
 package com.gigster.skymarket.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
-    //todo order confirmation email
+    @Autowired
+    private JavaMailSender mailSender;
 
+
+    @Async("asyncExecutor")
+    public void sendMail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
     //todo order confirmation message sms
+
 }
+
+
+
