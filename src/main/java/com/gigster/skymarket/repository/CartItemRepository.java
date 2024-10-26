30cd8fd5d81056cf4f412b38a6f1c17496177776
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-26 23:01:53 +0300

    Added CartRepository and CartItemRepository

---
+++ src/main/java/com/gigster/skymarket/repository/CartItemRepository.java
@@ -0,0 +1,7 @@
+package com.gigster.skymarket.repository;
+
+import com.gigster.skymarket.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
