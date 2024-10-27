f157dff0cf45551b308cd40a2a0be3e3aefc12cd
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-27 13:37:04 +0300

    Changed method Parameters

--- src/main/java/com/gigster/skymarket/service/CartServiceItem.java
+++ src/main/java/com/gigster/skymarket/service/CartServiceItem.java
@@ -3,13 +3,14 @@ package com.gigster.skymarket.service;
 
 import com.gigster.skymarket.dto.CartItemDto;
 import com.gigster.skymarket.dto.ResponseDto;
+import com.gigster.skymarket.model.Cart;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface CartServiceItem {
     // Create: Add an item to the cart
-    ResponseEntity<ResponseDto> addItemToCart(Long cartId, CartItemDto cartItemDto);
+    ResponseEntity<ResponseDto> addItemToCart(Cart cart, CartItemDto cartItemDto);
 
     // Read: Get all items in the cart
     ResponseEntity<List<CartItemDto>> getCartItems(Long cartId);
