46b78b9cf61d655b8abe8a3c11fd19ac30bbf2c9
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-31 03:47:26 +0300

    feat: created CategoryName and implemented it on Category

---
+++ src/main/java/com/gigster/skymarket/enums/CategoryName.java
@@ -0,0 +1,12 @@
+package com.gigster.skymarket.enums;
+
+public enum CategoryName {
+    ELECTRONICS,
+    GROCERIES,
+    CLOTHING,
+    FURNITURE,
+    STATIONARY;
+    public static CategoryName fromString(String categoryName) {
+        return CategoryName.valueOf(categoryName.toUpperCase());
+    }
+}
