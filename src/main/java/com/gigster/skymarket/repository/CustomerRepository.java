8b2027cfceb22e642c8011e9c6459ca79005b477
fsdkibe <duncancheruiyot93@gmail.com>
2024-10-31 17:38:40 +0300

    refact: removed unused User import from CustomerRepository

--- src/main/java/com/gigster/skymarket/repository/CustomerRepository.java
+++ src/main/java/com/gigster/skymarket/repository/CustomerRepository.java
@@ -1,7 +1,6 @@
 package com.gigster.skymarket.repository;
 
 import com.gigster.skymarket.model.Customer;
-import com.gigster.skymarket.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
