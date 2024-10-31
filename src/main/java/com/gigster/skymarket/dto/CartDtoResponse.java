aa69c5b9dd6d5e9a423aabca4d6b40e6e9d8200f
cheruiyotcollins <kelvincollins86@gmail.com>
2024-10-31 03:43:42 +0300

    feat-Create CartDtoResponse

---
+++ src/main/java/com/gigster/skymarket/dto/CartDtoResponse.java
@@ -0,0 +1,18 @@
+package com.gigster.skymarket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartDtoResponse {
+    private String name;
+    private List<CartItemDtoResponse> cartItemDtoList;
+    private double totalPrice;
+}
