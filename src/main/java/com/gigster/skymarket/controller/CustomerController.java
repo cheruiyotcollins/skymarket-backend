d33c8263faaa9ffb49b2197bd27d6bc919e299b1
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-01 06:47:33 +0300

    upd: Added update Custoemr API

--- src/main/java/com/gigster/skymarket/controller/CustomerController.java
+++ src/main/java/com/gigster/skymarket/controller/CustomerController.java
@@ -1,41 +1,42 @@
 package com.gigster.skymarket.controller;
 
-import com.gigster.skymarket.dto.CustomerResponseDto;
-import com.gigster.skymarket.dto.NewCustomerDto;
+import com.gigster.skymarket.dto.CustomerDto;
 import com.gigster.skymarket.dto.ResponseDto;
 import com.gigster.skymarket.service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/customers")
 @Slf4j
 public class CustomerController {
     @Autowired
-    CustomerService customerService;
+    private CustomerService customerService;
 
     @PostMapping
-    public ResponseEntity<ResponseDto> createCustomer(@Valid @RequestBody NewCustomerDto newCustomer) {
-
+    public ResponseEntity<ResponseDto> createCustomer(@Valid @RequestBody CustomerDto newCustomer) {
         return customerService.saveCustomer(newCustomer);
     }
 
     @GetMapping
-    //todo pagination
+    // TODO: Add pagination
     public ResponseEntity<ResponseDto> getAllCustomers() {
         return customerService.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResponseDto> getCustomerById(@PathVariable long id) {
-        // Presumably, you'll have logic here to build the response
         return customerService.findCustomerById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseDto> updateCustomer(
+            @PathVariable long id,
+            @Valid @RequestBody CustomerDto updatedCustomer) {
+        return customerService.updateCustomer(id, updatedCustomer);
     }
 
     @DeleteMapping("/{id}")
