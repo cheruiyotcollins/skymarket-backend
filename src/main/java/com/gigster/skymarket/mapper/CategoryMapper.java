388c48947bc39342e525612a3b270a9eba8a2a64
fsdkibe <duncancheruiyot93@gmail.com>
2024-11-03 01:17:11 +0300

    feat: create CategoryMapper interface for DTO-entity mapping

---
+++ src/main/java/com/gigster/skymarket/mapper/CategoryMapper.java
@@ -0,0 +1,20 @@
+package com.gigster.skymarket.mapper;
+
+import com.gigster.skymarket.dto.CategoryDto;
+import com.gigster.skymarket.enums.CategoryName;
+import com.gigster.skymarket.model.Category;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+    CategoryDto toDto(Category category);
+
+    Category toEntity(CategoryDto categoryDto);
+
+    List<CategoryDto> toDtoList(List<Category> categories);
+
+    List<Category> toEntityList(List<CategoryDto> categoryDtos);
+}
