99d6e3ab1a73cff0694f85398a36753a4d44b306
fsdkibe <duncancheruiyot93@gmail.com>
2024-11-03 01:33:01 +0300

    feat: enhance CategoryService interface with paginated support

--- src/main/java/com/gigster/skymarket/service/CategoryService.java
+++ src/main/java/com/gigster/skymarket/service/CategoryService.java
@@ -1,15 +1,21 @@
 package com.gigster.skymarket.service;
 
 import com.gigster.skymarket.dto.ResponseDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+
 
 public interface CategoryService{
     ResponseEntity<ResponseDto> addCategory(String categoryName);
 
-    ResponseEntity<ResponseDto> getAll();
+    ResponseEntity<ResponseDto> getAllCategories(Pageable pageable);
+
+    ResponseEntity<ResponseDto> findById(Long categoryId);
+
     ResponseEntity<ResponseDto> updateCategory(Long categoryId, String newCategoryName);
 
     ResponseEntity<ResponseDto> deleteCategory(Long categoryId);
 
-    ResponseEntity<ResponseDto> findById(Long categoryId);
+
+
 }
