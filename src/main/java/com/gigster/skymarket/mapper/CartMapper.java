058911559b152114b1aea3db503fe31d22cf627e
fsdkibe <duncancheruiyot93@gmail.com>
2024-11-04 18:11:55 +0300

    feat: create CartMapper interface for mapping Cart entities and DTOs

---
+++ src/main/java/com/gigster/skymarket/mapper/CartMapper.java
@@ -0,0 +1,19 @@
+package com.gigster.skymarket.mapper;
+
+import com.gigster.skymarket.dto.CartDto;
+import com.gigster.skymarket.model.Cart;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CartMapper {
+
+    CartDto toDto(Cart cart);
+
+    Cart toEntity(CartDto cartDto);
+
+    List<CartDto> toDtoList(List<Cart> carts);
+
+    List<Cart> toEntityList(List<CartDto> cartDtos);
+}
