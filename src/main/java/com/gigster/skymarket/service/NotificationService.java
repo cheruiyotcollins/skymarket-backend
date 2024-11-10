108681638844e96234c748bd8d581d6a688d866b
fsdkibe <duncancheruiyot93@gmail.com>
2024-11-10 17:35:12 +0300

    refact: update NotificationService interface for future SMS support

--- src/main/java/com/gigster/skymarket/service/NotificationService.java
+++ src/main/java/com/gigster/skymarket/service/NotificationService.java
@@ -2,10 +2,10 @@ package com.gigster.skymarket.service;
 
 public interface NotificationService {
 
+    // Method to send an email
     void sendMail(String to, String subject, String body);
 
-    // todo add sms notification
+//    TODO: To be implemented in future(current option is Twilio).
+//    // Method to send SMS
+//    void sendSms(String to, String message);
 }
-
-
-
