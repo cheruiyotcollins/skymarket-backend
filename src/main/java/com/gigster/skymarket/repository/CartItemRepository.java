9a0b8e79dbb1e5777a13077c7ad63645c69414b1
fsdkibe <duncancheruiyot93@gmail.com>
2024-11-11 23:50:19 +0300

    feat: update CartItemRepository to support pagination and enhanced lookup by cart and item IDs

--- src/main/java/com/gigster/skymarket/repository/CartItemRepository.java
+++ src/main/java/com/gigster/skymarket/repository/CartItemRepository.java
@@ -1,12 +1,16 @@
 package com.gigster.skymarket.repository;
 
-import com.gigster.skymarket.model.Cart;
 import com.gigster.skymarket.model.CartItem;
-import com.gigster.skymarket.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
+    Optional<CartItem> findByCartIdAndItemId(Long cartId, Long itemId);
+
+    Page<CartItem> findByCartId(Long cartId, Pageable pageable);
 }
