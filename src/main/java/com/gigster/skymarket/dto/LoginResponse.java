fee5f093eab6b3246b9e38fa9906d174b030a557
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-11 18:41:33 +0300

    feat: Added LoginResponse Dto

---
+++ src/main/java/com/gigster/skymarket/dto/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.gigster.skymarket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@Setter
+public class LoginResponse {
+    private String token;
+    private boolean firstLogin;
+
+    public LoginResponse(String token, boolean firstLogin) {
+        this.token = token;
+        this.firstLogin = firstLogin;
+    }
+
+}
+
