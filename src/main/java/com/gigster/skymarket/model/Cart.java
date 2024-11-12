accfadd26a198b2629b10203f26508a1c7ab87da
fsdkibe <duncancheruiyot93@gmail.com>
2024-11-12 16:38:58 +0300

    feat: add totalPrice field to Cart model

--- src/main/java/com/gigster/skymarket/model/Cart.java
+++ src/main/java/com/gigster/skymarket/model/Cart.java
@@ -24,5 +24,5 @@ public class Cart {
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CartItem> cartItems = new ArrayList<>();
 
+    private double totalPrice;
 }
-
