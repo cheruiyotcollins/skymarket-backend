43c610b7289eae941dd036ea724423bedf24a8e7
fsdkibe <duncancheruiyot93@gmail.com>
2024-11-12 16:35:57 +0300

    refact: update CartRepository to use customerId instead of Customer object

--- src/main/java/com/gigster/skymarket/repository/CartRepository.java
+++ src/main/java/com/gigster/skymarket/repository/CartRepository.java
@@ -1,11 +1,10 @@
 package com.gigster.skymarket.repository;
 
 import com.gigster.skymarket.model.Cart;
-import com.gigster.skymarket.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    Optional<Cart> findByCustomer(Customer customer);
+    Optional<Cart> findByCustomerId(Long customer);
 }
