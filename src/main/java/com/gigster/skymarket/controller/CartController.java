4bfaece8bd060c78e20ed7cf7a1e2f15deb9cb24
fsdkibe <duncancheruiyot93@gmail.com>
2024-11-12 16:42:10 +0300

    feat: add remove and clear cart endpoints in CartController

--- src/main/java/com/gigster/skymarket/controller/CartController.java
+++ src/main/java/com/gigster/skymarket/controller/CartController.java
@@ -8,6 +8,7 @@ import com.gigster.skymarket.service.CartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -33,10 +34,24 @@ public class CartController {
         return cartService.getAllCarts(pageable);
     }
 
-    @GetMapping("/customer")
+    @GetMapping("/customerId")
     public ResponseEntity<ResponseDto> findCartByCustomerId(Authentication authentication){
         String username=authentication.getName();
         return cartService.findCartPerCustomer(username);
     }
 
+    @DeleteMapping("/remove/{productId}")
+    public ResponseEntity<ResponseDto> removeItemFromCart(@PathVariable Long productId, Authentication authentication) {
+        UserPrincipal userPrincipal = CurrentUserV2.getCurrentUser();
+        return ResponseEntity.ok(cartService.removeItemFromCart(productId).getBody());
+    }
+
+    @DeleteMapping("/clear/{customerId}")
+    public ResponseEntity<ResponseDto> clearCart(@PathVariable Long customerId) {
+        return cartService.clearCart(customerId);
+    }
+
 }
+
+
+
