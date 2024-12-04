b0d0fa3e8059b89b28e2d839e49eb6f2a78e739a
fsdkibe <duncancheruiyot93@gmail.com>
2024-12-04 21:53:41 +0300

    feat: enhance Customer entity with cart association and constraints

--- src/main/java/com/gigster/skymarket/model/Customer.java
+++ src/main/java/com/gigster/skymarket/model/Customer.java
@@ -1,6 +1,5 @@
 package com.gigster.skymarket.model;
 
-
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -8,22 +7,22 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name= "customers")
+@Table(name = "customers")
 public class Customer implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "full_name")
+    @Column(name = "full_name", nullable = false)
     private String fullName;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "phone_no")
@@ -31,6 +30,9 @@ public class Customer implements Serializable {
 
     @Column(name = "gender")
     private String gender;
+
+    @OneToOne(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Cart cart;
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Order> orders = new ArrayList<>();
