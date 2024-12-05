a5510404547b2caf2f73e0a45cc4fe60208d1476
fsdkibe <duncancheruiyot93@gmail.com>
2024-12-05 17:47:03 +0300

    refact: update findByCustomerId in CartRepository to use JPQL for improved query structure

--- src/main/java/com/gigster/skymarket/repository/CartRepository.java
+++ src/main/java/com/gigster/skymarket/repository/CartRepository.java
@@ -2,9 +2,14 @@ package com.gigster.skymarket.repository;
 
 import com.gigster.skymarket.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    Optional<Cart> findByCustomerId(Long customer);
+    @Query("SELECT c FROM Cart c WHERE c.customer.customerId = :customerId")
+    Optional<Cart> findByCustomerId(@Param("customerId") Long customerId);
+
 }
