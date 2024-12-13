509a1b464b2600bc349500bc46c2891e56760cac
fsdkibe <duncancheruiyot93@gmail.com>
2024-12-13 23:37:44 +0300

    refact: update AdminConfig to handle ROLE_ADMIN and set roles for admin user

--- src/main/java/com/gigster/skymarket/config/AdminConfig.java
+++ src/main/java/com/gigster/skymarket/config/AdminConfig.java
@@ -1,6 +1,7 @@
 package com.gigster.skymarket.config;
 
 import com.gigster.skymarket.enums.RoleName;
+import com.gigster.skymarket.model.Role;
 import com.gigster.skymarket.model.User;
 import com.gigster.skymarket.repository.RoleRepository;
 import com.gigster.skymarket.repository.UserRepository;
@@ -8,6 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 @Slf4j
@@ -27,11 +31,21 @@ public class AdminConfig implements CommandLineRunner {
     public void run(String... args) {
         log.info("Checking if an admin user exists");
 
-        boolean noAdminsExist = userRepository.countByRoleName(RoleName.ADMIN) == 0;
+        boolean noAdminsExist = userRepository.countByRoleName(RoleName.ROLE_ADMIN) == 0;
 
         if (noAdminsExist) {
+            // Check if the ROLE_ADMIN role exists
+            Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN).orElse(null);
+            if (adminRole == null) {
+                log.error("ROLE_ADMIN does not exist in the database. Please insert it manually.");
+                return;  // Exit early if the role is not found
+            }
+
             User user = new User();
-            user.setRole(roleRepository.findByName(RoleName.ADMIN).orElseThrow());
+            Set<Role> roles = new HashSet<>();
+            roles.add(adminRole);  // Add the admin role
+            user.setRoles(roles);
+
             user.setFullName("Admin");
             user.setEmail("admin@gmail.com");
             user.setUsername("admin");
