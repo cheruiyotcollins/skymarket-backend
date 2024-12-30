94be7b9b9cd01480da23e47362e143f1a963cff5
fsdkibe <duncancheruiyot93@gmail.com>
2024-12-30 22:19:45 +0300

    feat: add CatalogueService interface

---
+++ src/main/java/com/gigster/skymarket/service/CatalogueService.java
@@ -0,0 +1,9 @@
+package com.gigster.skymarket.service;
+
+import com.gigster.skymarket.dto.ResponseDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+public interface CatalogueService {
+
+}
