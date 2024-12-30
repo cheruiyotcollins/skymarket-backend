fbef516b38a6fdf027273f7457bbaeb2bda6f164
fsdkibe <duncancheruiyot93@gmail.com>
2024-12-30 22:05:39 +0300

    refact: replace @Getter and @Setter with @Data in LoginResponse

--- src/main/java/com/gigster/skymarket/dto/LoginResponse.java
+++ src/main/java/com/gigster/skymarket/dto/LoginResponse.java
@@ -1,10 +1,8 @@
 package com.gigster.skymarket.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class LoginResponse {
     private String token;
     private boolean firstLogin;
