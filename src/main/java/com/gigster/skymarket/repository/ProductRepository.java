e13fd99fec8caa863c1846d866b9621491115074
fsdkibe <duncancheruiyot93@gmail.com>
2024-12-30 22:13:57 +0300

    feat: add pageable support for finding products by productId in ProductRepository

--- src/main/java/com/gigster/skymarket/repository/ProductRepository.java
+++ src/main/java/com/gigster/skymarket/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.gigster.skymarket.repository;
 
 import com.gigster.skymarket.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
@@ -25,5 +27,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("UPDATE Product p SET p.stock = p.stock - :quantity WHERE p.id = :productId AND p.stock >= :quantity")
     int updateStockIfAvailable(@Param("productId") Long productId, @Param("quantity") int quantity);
 
+    Page<Product> findByProductId(Long productId, Pageable pageable);
 
 }
