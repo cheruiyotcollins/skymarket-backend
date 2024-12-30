f47b967253a239be6ba37aeffb182785c00612b5
fsdkibe <duncancheruiyot93@gmail.com>
2024-12-30 22:07:46 +0300

    refact: update ProductDto to include categoryId and productId, remove id field

--- src/main/java/com/gigster/skymarket/dto/ProductDto.java
+++ src/main/java/com/gigster/skymarket/dto/ProductDto.java
@@ -6,11 +6,11 @@ import lombok.Data;
 @Data
 @Builder
 public class ProductDto {
-    private Long id;
     private String name;
     private String description;
     private Double price;
-    private String category;
+    private Long categoryId;
     private String imageUrl;
+    private Long productId;
 
 }
