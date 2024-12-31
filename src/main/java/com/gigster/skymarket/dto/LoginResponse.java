98e4f5677cf9eb8e8e0409192bfa289e3bb3a24e
cheruiyotcollins <kelvincollins86@gmail.com>
2024-12-31 07:13:45 +0300

    upd: updated LoginResponse variables added success and message variables

--- src/main/java/com/gigster/skymarket/dto/LoginResponse.java
+++ src/main/java/com/gigster/skymarket/dto/LoginResponse.java
@@ -1,15 +1,29 @@
 package com.gigster.skymarket.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+@NoArgsConstructor
 public class LoginResponse {
     private String token;
     private boolean firstLogin;
+    private int success;
+    private String message;
+
 
     public LoginResponse(String token, boolean firstLogin) {
-        this.token = token;
-        this.firstLogin = firstLogin;
+        this.firstLogin=firstLogin;
+        this.token=token;
     }
-
+    public LoginResponse(String token, boolean firstLogin, int success,String message) {
+        this.firstLogin=firstLogin;
+        this.token=token;
+        this.message=message;
+    }
 }
