9d95622f6c2a5bdc266ff122931498c616c0ab0d
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-01 22:27:09 +0300

    refact: update OrderController to use versioned API endpoint

--- src/main/java/com/gigster/skymarket/controller/OrderController.java
+++ src/main/java/com/gigster/skymarket/controller/OrderController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/api/v1/orders")
 @Slf4j
 public class OrderController {
 
