e2c88cb0a7578d6ecd57522c331264835b2f9a90
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-01 22:26:48 +0300

    refact: update CustomerController to use versioned API endpoint

--- src/main/java/com/gigster/skymarket/controller/CustomerController.java
+++ src/main/java/com/gigster/skymarket/controller/CustomerController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/customers")
+@RequestMapping(value = "/api/v1/customers")
 @Slf4j
 public class CustomerController {
     @Autowired
