952589356554efcb4f7c14e9ac72564c1b018342
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-03 21:50:18 +0300

    refact: rename id field in Category model to categoryId

--- src/main/java/com/gigster/skymarket/model/Category.java
+++ src/main/java/com/gigster/skymarket/model/Category.java
@@ -15,7 +15,7 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long categoryId;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, unique = true)
