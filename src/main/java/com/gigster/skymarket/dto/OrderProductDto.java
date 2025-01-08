6ad0b232d5eed1385ac4a01f7cfc8ddb6f801633
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-08 19:04:32 +0300

    feat: add Lombok annotations for constructor, builder, and no-args constructor in OrderProductDto

--- src/main/java/com/gigster/skymarket/dto/OrderProductDto.java
+++ src/main/java/com/gigster/skymarket/dto/OrderProductDto.java
@@ -1,8 +1,14 @@
 package com.gigster.skymarket.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OrderProductDto {
     private Long productId;
     private int quantity;
