9f0de98b12d3d5099505111a9af089ad8e39590a
cheruiyotcollins <kelvincollins86@gmail.com>
2025-01-09 17:33:21 +0300

    feat: Added validation constrain on ChangePasswordDto

--- src/main/java/com/gigster/skymarket/dto/ChangePasswordDto.java
+++ src/main/java/com/gigster/skymarket/dto/ChangePasswordDto.java
@@ -1,9 +1,11 @@
 package com.gigster.skymarket.dto;
 
 import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 public class ChangePasswordDto {
     private String oldPassword;
+    @NotBlank(message = "New password must not be blank")
     private String newPassword;
 }
