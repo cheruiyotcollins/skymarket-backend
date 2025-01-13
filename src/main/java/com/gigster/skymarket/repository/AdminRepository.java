ac29a0758ae739667d30f4eec32228d8b2025847
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-13 10:11:43 +0300

    feat: create AdminRepository for database operations

---
+++ src/main/java/com/gigster/skymarket/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.gigster.skymarket.repository;
+
+import com.gigster.skymarket.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+    Admin findByEmail(String email);
+}
