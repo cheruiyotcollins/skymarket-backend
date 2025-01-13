494687c1789b29761797bd6ad0017bae207a14c9
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-13 10:15:47 +0300

    feat: create SuperAdminDto for super admin data transfer

---
+++ src/main/java/com/gigster/skymarket/dto/SuperAdminDto.java
@@ -0,0 +1,16 @@
+package com.gigster.skymarket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuperAdminDto {
+
+    private Long superAdminId;
+    private String superAdminName;
+    private String email;
+    private String employeeNo;
+    private String contact;
+}
