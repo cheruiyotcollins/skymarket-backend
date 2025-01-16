ef5a4659db6a8070a7f0a1c0a4d42953b4dfbc5e
cheruiyotcollins <kelvincollins86@gmail.com>
2025-01-16 08:38:56 +0300

    feat: Payments service providers implementation initial structure

---
+++ src/main/java/com/gigster/skymarket/service/PaymentServiceProviderService.java
@@ -0,0 +1,5 @@
+package com.gigster.skymarket.service;
+
+
+public interface PaymentServiceProviderService {
+}
---
+++ src/main/java/com/gigster/skymarket/service/impl/PaymentServiceProviderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.gigster.skymarket.service.impl;
+
+import com.gigster.skymarket.service.PaymentServiceProviderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceProviderServiceImpl implements PaymentServiceProviderService {
+}
