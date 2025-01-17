9f23da414c9dec8c91884bc905fb71fae9c083d3
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-17 16:02:36 +0300

    chore: addition of missing annotation

--- src/main/java/com/gigster/skymarket/controller/PaymentServiceProviderController.java
+++ src/main/java/com/gigster/skymarket/controller/PaymentServiceProviderController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/payment-service-providers")
 public class PaymentServiceProviderController {
