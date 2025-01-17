93ac8e5f875e6bd39a497091616650860a7e1e02
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-17 23:36:26 +0300

    chore: reinstated the test annotation

--- src/test/java/com/gigster/skymarket/SkymarketApplicationTests.java
+++ src/test/java/com/gigster/skymarket/SkymarketApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 //TODO: To research on how to have this annotation intact and not cause the error,
 // where the test's application.properties is failing to be incorporated well in to the system.
-//@SpringBootTest
+@SpringBootTest
 class SkymarketApplicationTests {
 
 	@Test
