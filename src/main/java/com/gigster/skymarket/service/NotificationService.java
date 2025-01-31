7fd2a6005c6c68c4a8744c6ac6277d35bf79e96a
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-31 20:54:26 +0300

    feat: extend NotificationService with order and payment notification methods

--- src/main/java/com/gigster/skymarket/service/NotificationService.java
+++ src/main/java/com/gigster/skymarket/service/NotificationService.java
@@ -1,9 +1,19 @@
 package com.gigster.skymarket.service;
 
+import com.gigster.skymarket.model.Customer;
+import com.gigster.skymarket.model.Order;
+import com.gigster.skymarket.model.Payment;
+
 public interface NotificationService {
 
     // Method to send an email
     void sendMail(String to, String subject, String body);
+
+    void sendOrderConfirmationEmail(Customer customer, Order order);
+
+    void sendPaymentFailureNotification(Customer customer, Payment payment);
+
+    void sendPaymentConfirmationNotification(Customer customer, Payment payment);
 
 //    TODO: To be implemented in future(current option is Twilio).
 //    Method to send SMS
