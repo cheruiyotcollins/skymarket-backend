38acd0657c61f5caa6c5da002a532086bf9c79b2
fsdkibe <duncancheruiyot93@gmail.com>
2025-01-31 22:05:46 +0300

    chore: code clean up

--- src/main/java/com/gigster/skymarket/repository/PaymentServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gigster.skymarket.repository;
-
-import com.gigster.skymarket.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentServiceRepository extends JpaRepository<Payment, Long> {
-}
