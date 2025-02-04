ca0d095ee4bda363a4b43b06ff2625adce666967
fsdkibe <duncancheruiyot93@gmail.com>
2025-02-04 19:13:31 +0300

    chore: code cleanup

--- src/main/java/com/gigster/skymarket/controller/PaymentController.java
+++ src/main/java/com/gigster/skymarket/controller/PaymentController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-        import java.math.BigDecimal;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/v1/payments")
