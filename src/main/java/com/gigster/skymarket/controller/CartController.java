4cfa8eacb9b19460ac4a773d4faa757547671975
fsdkibe <duncancheruiyot93@gmail.com>
2025-02-18 18:17:08 +0300

    chore: code clean up

--- src/main/java/com/gigster/skymarket/controller/CartController.java
+++ src/main/java/com/gigster/skymarket/controller/CartController.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
