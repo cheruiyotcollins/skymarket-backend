738a613891890ea5fa4ecdb6d34db7f1247a47f1
fsdkibe <duncancheruiyot93@gmail.com>
2025-02-20 23:16:37 +0300

    chore: code clean up

--- src/main/java/com/gigster/skymarket/dto/CartItemDto.java
+++ src/main/java/com/gigster/skymarket/dto/CartItemDto.java
@@ -6,9 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CartItemDto {
     private Long productId;
     private String productName;
