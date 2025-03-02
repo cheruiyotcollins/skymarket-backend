fb237d99af06e6f91b03abfee1c976e922eaee71
fsdkibe <duncancheruiyot93@gmail.com>
2025-03-03 02:24:50 +0300

    refact: optimize CatalogueController by improving pagination handling and service method calls

--- src/main/java/com/gigster/skymarket/controller/CatalogueController.java
+++ src/main/java/com/gigster/skymarket/controller/CatalogueController.java
@@ -42,8 +42,8 @@ public class CatalogueController {
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size,
             @RequestParam(value = "sort", defaultValue = "id,asc") String sort) {
-        Pageable pageable = PageRequest.of(page, size, Sort.by(sort.split(",")));
-        return catalogueService.getAllCatalogues(pageable);
+
+        return catalogueService.getAllCatalogues(page, size, sort);
     }
 
     @PutMapping("/{id}")
