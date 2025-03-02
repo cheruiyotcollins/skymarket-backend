98fb3b608a856858262b0f455df7982d4f5f2b5e
cheruiyotcollins <kelvincollins86@gmail.com>
2025-03-02 21:33:06 +0300

    upd: added corsConfigurationSource to fix frontend cart api call

--- src/main/java/com/gigster/skymarket/config/WebConfig.java
+++ src/main/java/com/gigster/skymarket/config/WebConfig.java
@@ -2,18 +2,20 @@ package com.gigster.skymarket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Allow your frontend
+                .allowedOrigins("http://localhost:3000")  // Allow frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization") // Expose the Authorization header
-                .allowCredentials(true); // Allow cookies or tokens
+                .allowedHeaders("Authorization", "Content-Type") // Allow Authorization header
+                .exposedHeaders("Authorization")
+                .allowCredentials(true); // Allow cookies/tokens
     }
 }
