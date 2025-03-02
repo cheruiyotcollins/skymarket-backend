2ef940a2884a2abde37cf4ec579d3786494512fb
fsdkibe <duncancheruiyot93@gmail.com>
2025-03-03 01:55:50 +0300

    refact: update SuperAdminService to support pagination and sorting for getAllSuperAdmins

--- src/main/java/com/gigster/skymarket/service/SuperAdminService.java
+++ src/main/java/com/gigster/skymarket/service/SuperAdminService.java
@@ -11,7 +11,7 @@ public interface SuperAdminService {
 
     ExtendedResDto getSuperAdminById(Long id);
 
-    ExtendedResDto getAllSuperAdmins();
+    ExtendedResDto getAllSuperAdmins(int page, int size, String sort);
 
     ExtendedResDto updateSuperAdminById(SuperAdminDto superAdminDto);
 
