e1da0c2902bcd608aa44c7ec8976d365894093a4
fsdkibe <duncancheruiyot93@gmail.com>
2025-03-10 09:32:12 +0300

    feat: add UserNotFoundException for handling missing user scenarios

---
+++ src/main/java/com/gigster/skymarket/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.gigster.skymarket.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
