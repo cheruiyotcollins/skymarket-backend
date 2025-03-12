dbe024c6a8015d3c3306364cc4bd81dd0333892f
fsdkibe <duncancheruiyot93@gmail.com>
2025-03-12 18:15:50 +0300

    feat: add method to delete cart items by cartId in CartItemRepository

--- src/main/java/com/gigster/skymarket/repository/CartItemRepository.java
+++ src/main/java/com/gigster/skymarket/repository/CartItemRepository.java
@@ -4,6 +4,9 @@ import com.gigster.skymarket.model.CartItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,7 +17,12 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Optional<CartItem> findByCart_CartIdAndProductId(Long cartId, Long productId);
 
     Page<CartItem> findByCart_CartId(Long cartId, Pageable pageable);
+
     Optional<CartItem> findByCart_CartIdAndId(Long cartId, Long cartItemId);
 
     Optional<CartItem> findByProductId(Long productId);
+
+    @Modifying
+    @Query("DELETE FROM CartItem c WHERE c.cart.cartId = :cartId")
+    void deleteByCartId(@Param("cartId") Long cartId);
 }
